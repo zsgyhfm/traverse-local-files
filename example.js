e5cf4507b64c13ae9852943fcb9c9d819cad4de7
最是光阴化浮沫 <zsgyhfm@icloud.com>
2017-04-24 22:17:49 +0800

    解决异步写入的情况 覆盖上一个 本地的写入文本的名称   增加判断文件类型

--- example.js
+++ example.js
@@ -1,11 +1,32 @@
 var path = require('path');
 var fs = require('fs');
-var tra = require('./test.js');
+var Tra = require('./traverse.js');
 
-//指定需要遍历的目录
-var abslot_path = path.resolve('../product');
 //传入目录路径  和  保存的文件位置
-tra(abslot_path)
+function getfiles(fp,callback){
+    var arr = []
+    var fp = path.normalize(fp);
+    fs.readdir(fp, function (err,files) {
+        files.forEach(function (val) {
+            arr.push(path.resolve('../product',val));
+        })
+        if(callback){
+            callback(arr)
+        }
+    })
+}
+//获取目录下的产品分类
+getfiles('../product',function (data) {
+    data.forEach(function (val) {
+       new Tra(val,'../test',['.json']);
+    })
+});
 
 
+function print(msg){
+    console.log(msg)
+}
 
+
+//指定单个目录--
+//new Tra('目录地址','输出文件保存地址');
